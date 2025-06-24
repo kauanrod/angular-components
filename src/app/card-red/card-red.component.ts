300035cfaaeef818f66d3c67dafb0d9b96d8e61c
Kauan Rodrigues <kauanrod@hotmail.com>
2025-06-24 10:05:13 -0300

    setting values in html using objects

--- src/app/card-red/card-red.component.ts
+++ src/app/card-red/card-red.component.ts
@@ -6,15 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-red.component.scss']
 })
 export class CardRedComponent {
-  type: string = 'Basic';
-  price: number = 100;
-
-  getFullPrice(): string {
-    // setTimeout(() => {
-    //   console.log('Changing price to complete for a test');
-    //   this.type = 'Complete';
-    //   this.price = 250;
-    // }, 4000);
-    return `U$${this.price.toFixed(2)}/Month`;
-  }
+    plan = {
+      data: {
+        type: 'Basic',
+        price: 100.00,
+      },
+    }
 }
