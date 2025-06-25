5efa6260d249c735e01e3b7ccf47393a8503a067
Kauan Rodrigues <kauanrod@hotmail.com>
2025-06-25 08:58:08 -0300

    using optional chaining for null or undefined calls

--- src/app/card-red/card-red.component.ts
+++ src/app/card-red/card-red.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 
+interface ICardRedData {
+  plan: IPlan;
+}
+
+interface IPlan {
+  type: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-card-red',
   templateUrl: './card-red.component.html',
   styleUrls: ['./card-red.component.scss']
 })
 export class CardRedComponent {
-    plan = {
-      data: {
-        type: 'Basic',
-        price: 100.00,
-      },
+  data: ICardRedData = {
+    plan: {
+      type: 'Basic',
+      price: 100.00
     }
+  }
 }
