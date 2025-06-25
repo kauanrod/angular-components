b98438162a8bd049ebb8464f28ce8d8aa39c2283
Kauan Rodrigues <kauanrod@hotmail.com>
2025-06-25 14:17:19 -0300

    changing templateUrl for template since the html is just one line

--- src/app/card-red-button/card-red-button.component.ts
+++ src/app/card-red-button/card-red-button.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-red-button',
-  templateUrl: './card-red-button.component.html',
+  // templateUrl: './card-red-button.component.html',
+  template: `
+    <div class="card-red-button">Purchase</div>
+  `,
   styleUrls: ['./card-red-button.component.scss']
 })
 export class CardRedButtonComponent {
