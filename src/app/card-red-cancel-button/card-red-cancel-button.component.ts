20cb796e209098c0ebcb74d978ae1134d3b51f5d
Kauan Rodrigues <kauanrod@hotmail.com>
2025-06-29 10:20:14 -0300

    Organizing the project and importing styles with variables

--- src/app/card-red-cancel-button/card-red-cancel-button.component.ts
+++ src/app/card-red-cancel-button/card-red-cancel-button.component.ts
@@ -3,19 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-red-cancel-button',
   templateUrl: './card-red-cancel-button.component.html',
-  // styleUrls: ['./card-red-cancel-button.component.scss']
-  styles: [`
-    .card-red-cancel-button {
-      font-size: 16px;
-      text-align: center;
-      margin-top: 10px;
-      border-radius: 25px;
-      background-color: #d05656;
-      cursor: pointer;
-      font-weight: bold;
-      padding: 10px;
-    }
-  `]
+  styleUrls: ['./card-red-cancel-button.component.scss']
 })
 export class CardRedCancelButtonComponent {
 
